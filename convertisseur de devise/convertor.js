d6b993410afc8cf3498a9cbf783c0c7b62bf8146
mamadou <128175492+mamadoumben@users.noreply.github.com>
2023-04-09 11:13:55 -0700

    Add files via upload

---
+++ convertisseur de devise/convertor.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const change = 655.96;
+    const devisefr = document.getElementById('devisefr');
+    const deviseeu = document.getElementById('deviseeu');
+  
+    devisefr.addEventListener('keyup', function(){
+      let franc = parseFloat(this.value.trim());
+      let euro = parseFloat(franc/change);
+      //devisefr.value = franc.toFixed(2);
+      deviseeu.value = euro.toFixed(2);
+    });
+  
+    deviseeu.addEventListener('keyup', function(){
+      let euro = parseFloat(this.value.trim());
+      let franc = parseFloat(euro*change);
+      devisefr.value = franc.toFixed(2);
+      //deviseeu.value = euro.toFixed(2);
+    });
+  });  
